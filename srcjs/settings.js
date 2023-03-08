a637c78741d4e75902daec396b52101c5225498b
mauromiguelm <mauromiguelm@hotmail.com>
2023-03-08 17:22:51 +0100

    code cleaning

--- srcjs/settings.js
+++ srcjs/settings.js
@@ -33,23 +33,14 @@ export const handleSettings = () => {
   var lockState = false; //false will lock settings upon lock click
 
   $(".settings-lock").click(function(){
-    console.info("click event triggered")
-
+    // click listener to change the locked and unlocked settings
       if (lockState === false) {
         $('#settings-container').removeClass("settings-unlocked");
         $('#settings-container').addClass("settings-locked");
         $('.settings-locked-icon').removeClass("fa-lock-open");
         $('.settings-locked-icon').addClass("fa-lock");
-        //$('.settings-lock').removeClass("settings-lock-collapsed");
-        //$('.settings-lock').addClass("settings-lock-expanded");
         $('#settings-container').off("mouseleave");
-        
-        //settingsExpand();
-        //$container.show();
-        //$('.tab-settings').show();
         lockState = true;
-        console.info("lock locked");
-        
       } else {
         $('#settings-container').removeClass("settings-locked");
         $('#settings-container').addClass("settings-unlocked");
@@ -60,36 +51,22 @@ export const handleSettings = () => {
         })
         $('.settings-locked-icon').removeClass("fa-lock");
         $('.settings-locked-icon').addClass("fa-lock-open");
-        //settingsExpand();
-        //$container.show();
-        //$('.tab-settings').show();
         lockState = false;
-        console.info("lock unlocked, should not be working");
      }
   });
 }
-
-var mouseleaveHandler = function() {
-  settingsCollapse();
-  $container.hide();
-  $('.tab-settings').hide();
-};
 
 const settingsExpand = () => {
   //change settings sidebar css upon expanding
   $('.settings-lock').show(); // show lock when settings sidebar is expanded
   $('#settings-container').removeClass('settings-collapsed');
-  $('#settings-container').removeClass('settings-expanded');
   $('#settings-container').addClass('settings-expanded');
   //change icon css style for expanded position
   $('.settings-lock').removeClass('settings-lock-collapsed');
-  $('.settings-lock').removeClass('settings-lock-expanded');
   $('.settings-lock').addClass('settings-lock-expanded');
   $('.settings').addClass('p-2');
   $('.settings').removeClass('mt-3');
-  //$('.settings').toggleClass('p-2');
   toggleCollapseLabel();
-  //toggleCollapseContent();
 }
 
 const settingsCollapse = () => {
@@ -101,23 +78,8 @@ const settingsCollapse = () => {
   $('.settings').removeClass('p-2');
   $('.settings').addClass('mt-3');
   $('.settings-lock').hide(); // lock should be hidden when settings is collapsed
-  //not sure what this does
-  //$('.settings').toggleClass('p-2');
   toggleCollapseLabel();
-  //toggleCollapseContent();
 }
-
-// const toggleCollapseContent = () => {
-//   let $container = $('#settings-container')
-//     .find('.settings-content');
-
-//   if(isExpanded()) {
-//     $container.show();
-//     return
-//   }
-
-//   $container.hide();
-// }
 
 const toggleCollapseLabel = () => {
   let css = {
@@ -131,8 +93,6 @@ const toggleCollapseLabel = () => {
     'right': 'inherit',
     'transform': 'rotate(0deg)',
   }
-
-  //$('#settings-content').toggle();
 
   if(!isExpanded()) {
     css = {
@@ -160,20 +120,7 @@ const isExpanded = () => {
   return $('#settings-container').hasClass('settings-expanded');
 }
 
-// $(function(){
-//   setTimeout(() => {
-//     $('.settings-').trigger('click');
-//   }, 50);
-// });
-
-
 const moveSettings = () => {
-  // if(isMobile()){
-  //   $('.tab-settings').removeClass('d-none');
-  //   $('#settings-container').hide();
-  //   return;
-  // }
-
   $('.big-tab')
     .each((index, el) => {
       let settings = $(el)
